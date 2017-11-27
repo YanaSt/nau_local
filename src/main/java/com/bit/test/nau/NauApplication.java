c6b60dd73c689e9ef476497fc56d47435c0ac434
YanaSt <31668836+YanaSt@users.noreply.github.com>
2017-11-27 18:52:26 +0300

    create repozitorii
    создание репозитория

---
+++ src/main/java/com/bit/test/nau/NauApplication.java
@@ -0,0 +1,12 @@
+package com.bit.test.nau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bit/test/nau/test/Login.java
@@ -0,0 +1,4 @@
+package com.bit.test.nau.test;
+
+public class Login {
+}
